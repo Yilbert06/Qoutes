9b869f6a7e68304d9316830a14d5e3abb88e51c7
Yilbert Daniel Osorio Garmendia <yilbert06@gmail.com>
2023-03-02 02:34:24 -0300

    Perfection2.0

--- src/componentes/CardQuote.jsx
+++ src/componentes/CardQuote.jsx
@@ -6,10 +6,10 @@ const CardQuote = ({data, changeCards}) => {
   return (
     <section className="card">
       <div className="content">
-        <h2>FAMOUS QUOTES</h2>
+        <h2 className="tittle_quotes">FAMOUS QUOTES</h2>
         <p className="quotePharse">{data.quote}</p>
         <h3 className="autorName">
-          <strong className="autorName__str"><strong>Author:</strong></strong> {data.author}
+          <strong className="autorName__str">Author </strong>{data.author}
         </h3>
       </div>
       <Button changeCards={changeCards} />
